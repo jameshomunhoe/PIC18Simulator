8d52735d1f7087cc2f5b89181b9e30181f268f26
shyanfeng <shyanfeng@gmail.com>
2014-08-15 08:02:37 +0800

    Updated

--- src/ExecutionTable.c
+++ src/ExecutionTable.c
@@ -39,7 +39,25 @@ ExecutionTable executionTable[64] = {
 	[0x1b] = executeMOVWForNEGF,
 	[0x0d] = executeRLCF,
 	[0x10] = executeRRNCF,
-	[0x09] = executeADDWF
+	[0x09] = executeADDWF,
+	[0x08] = executeADDWFC,
+	[0x05] = executeANDWF,
+	[0x1A] = executeCLRF,
+	[0x07] = executeCOMF,
+	[0x18] = executeCPFSEQorexecuteCPFSLT,
+	[0x19] = executeCPFSGT,
+	[0x01] = executeDECF,
+	[0x0B] = executeDECFSZ,
+	[0x13] = executeDCFSNZ,
+	[0x0A] = executeINCF,
+	[0x0F] = executeINCFSZ,
+	[0x12] = executeINFSNZ,
+	[0x04] = executeIORWF,
+	[0x14] = executeMOVF,
+	[0x30] = executeMOVFF,
+	[0x31] = executeMOVFF,
+	[0x32] = executeMOVFF,
+	[0x33] = executeMOVFF,
 
 };
 
